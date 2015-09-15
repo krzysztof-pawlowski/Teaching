4cfb14e5aa6807cf46d9ebb488c2ae366d81e866
Krzysztof Pawlowski <krzysztof.pawlowski@gmail.com>
2015-09-15 22:33:31 +0200

    capital letters in list exercise

---
+++ Programming Methods/lab/LambdasAndStreams/src/main/java/pl/edu/pjatk/mpr/lambdas/streams/CapitalLetters.java
@@ -0,0 +1,20 @@
+package pl.edu.pjatk.mpr.lambdas.streams;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+/**
+ * Created by Krzysztof Pawlowski on 15/09/15.
+ */
+public class CapitalLetters {
+
+    /***
+     * Method creates a new list with capitalized words from the input list.Ó
+     * @param inputList The list of input words
+     * @return The list of capitalized input words.
+     */
+    public List<String> capitalizeWordsInList(List<String> inputList) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Programming Methods/lab/LambdasAndStreams/src/test/java/pl/edu/pjatk/mpr/lambdas/streams/CapitalLettersTest.java
@@ -0,0 +1,39 @@
+package pl.edu.pjatk.mpr.lambdas.streams;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by Krzysztof Pawlowski on 15/09/15.
+ */
+public class CapitalLettersTest {
+
+    private CapitalLetters capitalLetters;
+
+    @Before
+    public void before() {
+        capitalLetters = new CapitalLetters();
+    }
+
+    @Test
+    public void testCapitalizeWordsInList() {
+        // GIVEN
+        List<String> inputList = Arrays.asList("One", "two", "tHree");
+        List<String> expectedOutput = Arrays.asList("ONE", "TWO", "THREE");
+
+        // WHEN
+        List<String> result = capitalLetters.capitalizeWordsInList(inputList);
+
+        // THEN
+        assertThat(result, is(equalTo(expectedOutput)));
+    }
+
+
+}
